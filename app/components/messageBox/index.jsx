07bbe8cebfcabb4d9a1e6b0edf57913a4647991e
Samandar <yusupovvsamandar@gmail.com>
2023-09-07 20:06:54 +0500

    add patient login and some changes

---
+++ app/components/messageBox/index.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import List from "../lists";
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    lineHeight: '2',
+    overflow: "auto",
+    width: '100%'
+}));
+
+const lightTheme = createTheme({ palette: { mode: 'light' } });
+
+export default function Elevation({ primary, time, editor }) {
+    return (
+        // <ThemeProvider theme={lightTheme}>
+        <Item elevation={1}>
+            <List
+                primary={primary}
+                time={time}
+                editor={editor}
+            />
+        </Item>
+        // </ThemeProvider>
+    );
+}
---
+++ app/components/sendMessageBar/index.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SendIcon from '@mui/icons-material/Send';
+
+export default function CustomizedInputBase() {
+    const [inputText, setInputText] = React.useState('');
+
+    const handleSendUpdate = () => {
+        alert("good")
+    }
+
+    return (
+        <Paper
+            component="form"
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', position: 'fixed', bottom: '0', width: "100%" }}
+            onSubmit={(e) => {
+                e.preventDefault();
+                // Send the Message to the server
+                handleSendUpdate();
+            }}
+        >
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                value={inputText}
+                onChange={(e) => {
+                    setInputText(e.target.value);
+                }}
+                placeholder="Search Google Maps"
+                inputProps={{ 'aria-label': 'search google maps' }}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton onClick={handleSendUpdate} color="primary" sx={{ p: '10px' }} aria-label="directions">
+                <SendIcon />
+            </IconButton>
+        </Paper>
+    );
+}
